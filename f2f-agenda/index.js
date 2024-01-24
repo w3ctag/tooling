3ea9d0a3271587b8aa9be0b07908a1fbfd71c8aa
Lea Verou <lea@verou.me>
2024-01-24 14:47:14 +0000

    [f2f-agenda] Attempts to fix (WIP)

--- f2f-agenda/index.js
+++ f2f-agenda/index.js
@@ -1,6 +1,8 @@
 import GoogleSheets from "https://madata.dev/backends/google/sheets/google-sheets.js";
 import { Vue, SetData } from "https://mavue.mavo.io/mavue.js";
 
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/1D7low9ygKMXzzFcClTh5Q75JQwHTLdzpW0qPv1BSsUU/edit";
+
 let params = new URL(location).searchParams;
 
 globalThis.app = Vue.createApp({
@@ -57,35 +59,66 @@ globalThis.app = Vue.createApp({
 	}
 }).mount("#app_el");
 
-let f2f = params.get("f2f") ?? "Moonbase Alpha";
+let meeting = params.get("f2f");
+let isVirtual = false;
 
-let slots = await (new GoogleSheets("https://docs.google.com/spreadsheets/d/1D7low9ygKMXzzFcClTh5Q75JQwHTLdzpW0qPv1BSsUU/edit#gid=1326461427", {
-	range: "A6:X15", transpose: true, headerRow: true, sheet: `${f2f} Person x Breakout`
-})).load();
-let issues = (new GoogleSheets("https://docs.google.com/spreadsheets/d/1D7low9ygKMXzzFcClTh5Q75JQwHTLdzpW0qPv1BSsUU/edit#gid=1478762209", {
-headerRow: true, sheet: f2f
-})).load();
+if (!meeting) {
+	meeting = params.get("vf2f");
 
-// console.log(slots)
-
-let breakouts = {};
-let participants = new Set();
-
-// First, determine participants for each breakout
-for (let slot of slots) {
-	for (let participant in slot) {
-		let id = slot[participant];
-
-		if (id && id.toLowerCase() !== "x") {
-			let breakout = (breakouts[id] ??= {});
-			breakout.participants ??= [];
-			breakout.participants.push(participant);
-			participants.add(participant);
-		}
+	if (meeting) {
+		isVirtual = true;
 	}
 }
 
-issues = await issues;
+let [slots, issues, participants] = await Promise.all([
+	isVirtual ? GoogleSheets.load(SHEET_URL, {
+		range: "A6:X15", transpose: true, headerRow: true, sheet: `${meeting} Person x Breakout`
+	}) : null,
+	GoogleSheets.load(SHEET_URL, {
+		headerRow: true, sheet: meeting
+	}),
+	params.get("participants") ? GoogleSheets.load(SHEET_URL, {
+		headerRow: true, sheet: meeting + " Planning", range: params.get("participants")
+	}) : null,
+]);
+
+// console.log("Slots:", slots);
+
+let breakouts = {};
+let members = new Set();
+
+// First, determine participants for each breakout
+if (participants) {
+	for (let breakoutMeta of participants) {
+		let id = breakoutMeta.Slot;
+
+		if (!id) {
+			continue;
+		}
+
+		let breakout = (breakouts[id] ??= {});
+		breakout.participants = breakoutMeta.Who?.split(/,/).map(x => x.trim()).filter(x => /^[A-Z][a-z]+/.test(x) && x !== "All").sort() ?? [];
+		for (let participant of breakout.participants) {
+			members.add(participant);
+		}
+		breakout.location = breakoutMeta.Where;
+		breakout.goals = breakoutMeta.Goals;
+	}
+}
+else if (slots) {
+	for (let slot of slots) {
+		for (let participant in slot) {
+			let id = slot[participant];
+
+			if (id && id.toLowerCase() !== "x") {
+				let breakout = (breakouts[id] ??= {});
+				breakout.participants ??= [];
+				breakout.participants.push(participant);
+				members.add(participant);
+			}
+		}
+	}
+}
 
 // console.log(issues);
 
@@ -94,14 +127,11 @@ for (let issue of issues) {
 	let slot = issue.Slot;
 
 	if (!slot) {
+		// Issue not assigned to a slot, skip
 		continue;
 	}
 
-	if (!(slot in breakouts)) {
-		// Orphan breakout with no participants!
-		breakouts[slot] = {orphan: true};
-	}
-
+	breakouts[slot] ??= {};
 	let breakout = breakouts[slot];
 	breakout.issues ??= [];
 	breakout.issues.push({
@@ -109,9 +139,36 @@ for (let issue of issues) {
 		url: issue.URL,
 		comment: issue.Comments,
 	});
+
+	if (!breakout.participants) {
+		// We have not fetched slots, calculate participants from issues
+
+		if (!members.size) {
+			// First time, calculate which columns are participants
+			let keys = Object.keys(issue).filter(x => !["Slot", "Repo", "#", "Name", "URL", "Comments"].includes(x));
+			members = new Set(keys);
+		}
+
+		breakout.participants = new Set();
+	}
+
+	// If we have no explicit schedule, try to guess participants from issue
+	if (!participants && !slots) {
+		for (let participant of members) {
+			if (slot === "02b") console.log(issue.Name, participant, issue[participant])
+			if (issue[participant]) {
+				breakout.participants.add(participant);
+			}
+		}
+	}
+}
+
+for (let id in breakouts) {
+	let breakout = breakouts[id];
+	breakout.participants = [...breakout.participants].sort();
 }
 
 console.log(breakouts);
 
 app.breakouts = breakouts;
-app.participants = participants;
+app.participants = members;
